a5b54b28263cb4d3033eb68ca1850d41ea042b37
Alejandro <alejandrovasqueez@gmail.com>
2020-08-06 18:27:25 -0400

    Docs agregados

---
+++ docs/precache-manifest.3b7fd2b2a2931196a8e5e5c6e37eb926.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e1360579fa5d8e0314cddda54da9cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "248423f6dd6ead7ffd10",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "cd77d31d021301fb254f",
+    "url": "/static/css/main.c066af65.chunk.css"
+  },
+  {
+    "revision": "248423f6dd6ead7ffd10",
+    "url": "/static/js/2.1a68ba2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1a68ba2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd77d31d021301fb254f",
+    "url": "/static/js/main.4d2eac66.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
